e0c22ce022d7bb115633a320ad4b4e9f6671d9a0
ishaan-566 <ishaanaggarwal566@gmail.com>
2021-10-25 11:01:13 +0530

    Initial commit

---
+++ src/main/java/controller/ProfessorServlet.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.UserCredentials;
+
+/**
+ * Servlet implementation class ProfessorServlet
+ */
+@WebServlet("/ProfessorServlet")
+public class ProfessorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProfessorServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		HttpSession sn = request.getSession(false); 
+		UserCredentials obj = (UserCredentials) sn.getAttribute("user");
+		if(obj.getUsertype().equals("admin")) {
+			request.setAttribute("action", action);
+			RequestDispatcher rd = request.getRequestDispatcher("AdminServlet");
+			rd.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
